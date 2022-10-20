83b83780447d7818092328be9753ea8fc608b3d9
Erik Satriawan <satriawarn@gmail.com>
2022-10-20 09:10:55 +0700

    moving project 10

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project1
+//
+//  Created by MTMAC51 on 18/10/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
