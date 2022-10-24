250762765ac0e1a0a476d38e0f1a5e0dce7d565a
Erik Satriawan <satriawarn@gmail.com>
2022-10-24 15:42:30 +0700

    modify using animations CGAffineTranform

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -15,8 +15,10 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var highestScore = 0
     var correctAnswer = 0
-    var counter = 0
+    var questionsCounter = 0
+    let questionsLimit = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,8 +37,8 @@ class ViewController: UIViewController {
         
         askQuestion()
     }
-
-    func askQuestion(action: UIAlertAction! = nil) {
+    
+    func askQuestion(_ action: UIAlertAction! = nil) {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         
@@ -44,41 +46,83 @@ class ViewController: UIViewController {
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
-        title = "What is the flag of \(countries[correctAnswer].uppercased()), Score \(score)"
+        for button in [button1, button2, button3] {
+            button?.alpha = 0.0
+            button?.transform = CGAffineTransform(scaleX: 0.85, y: 0.85)
+        }
+        
+        UIView.animate(withDuration: 0.5) {
+            for button in [self.button1, self.button2, self.button3] {
+                button?.alpha = 1.0
+                button?.transform = .identity
+            }
+        }
+        
+        title = "\(countries[correctAnswer].uppercased()) - Score: \(score)"
+        
+    }
+    
+    func resetGame(_ action: UIAlertAction!) {
+        score = 0
+        questionsCounter = 0
+        askQuestion(action)
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         var title: String
+        var message: String
+        
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: []) {
+            sender.transform = CGAffineTransform(scaleX: 0.85, y: 0.85)
+        } completion: { (finish) in
+            sender.isHidden = false
+        }
+        
+        questionsCounter += 1
         
         if sender.tag == correctAnswer {
-            title = "Correct"
             score += 1
-            counter += 1
+            title = "Correct!"
+            message = "Your score is \(score)"
         } else {
-            title = "Wrong"
-            counter += 1
+            score -= 1
+            title = "Wrong!"
+            message = "That's the flag of \(countries[sender.tag].uppercased())"
         }
         
-        let ac = UIAlertController(title: title, message: "Your Score is \(score)", preferredStyle: .alert)
-        if counter < 5 {
-            if title.hasPrefix("Wrong"){
-                ac.addAction(UIAlertAction(title: "Wrong that's flag is \(countries[sender.tag].uppercased())", style: .default, handler: askQuestion))
+        let ac: UIAlertController!
+                
+                if questionsCounter == questionsLimit {
+                    var message = "You have answered \(questionsCounter) questions"
+                    if score > highestScore {
+                        message += "\nYou have beaten your best result! 🎉"
+                        highestScore = score
+                        
+                        let defaults = UserDefaults.standard
+                        defaults.set(highestScore, forKey: "highestScore")
+                    } else {
+                        message += "\nHighest score \(highestScore)"
+                    }
+                    
+                    ac = UIAlertController(title: "Your final score is \(score)",
+                        message: message,
+                        preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "Restart Game", style: .default, handler: resetGame))
+                } else {
+                    ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+                }
+                
                 present(ac, animated: true)
-            } else {
-                ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
-                present(ac, animated: true)
-            }
-        } else {
-            let enoughAlert = UIAlertController(title: "You're already guess \(counter) of flags", message: "Correct Flags: \(score)", preferredStyle: .alert)
-            enoughAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action: UIAlertAction!) in
-                self.score = 0
-                self.counter = 0
-                self.askQuestion()
-              })
-            )
-            present(enoughAlert, animated: true)
-        }
         
     }
+    
+    @IBAction func showScore(_ sender: UIBarButtonItem) {
+            let ac = UIAlertController(title: "Your score is \(score)!",
+                message: "You have answered \(questionsCounter) of \(questionsLimit) questions",
+                preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: nil))
+            present(ac, animated: true)
+        }
 }
 
